0a3a46c9f865706e008ccc10e56fc006f34842ef
Raghavelu <68513343+Raghavelu@users.noreply.github.com>
2025-05-02 16:57:11 +0530

    Update nixpkgs.nix

--- .nixpacks/nixpkgs.nix
+++ .nixpacks/nixpkgs.nix
@@ -3,8 +3,11 @@ pkgs.mkShell {
     buildInputs = [
         pkgs.python311
         pkgs.python311Packages.pip
-        pkgs.python311Packages.gunicorn  # Correct package path
+        pkgs.python311Packages.gunicorn
         pkgs.dejavu_fonts
         pkgs.fontconfig
+        pkgs.zlib
+        pkgs.stdenv.cc.cc.lib  # C compiler utilities
+        pkgs.linuxPackages.nvidia_x11  # For numpy acceleration
     ];
 }
