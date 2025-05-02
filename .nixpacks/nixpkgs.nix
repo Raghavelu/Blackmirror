015f9ed427e3c26d30dc88ff8b7527cd4571b84b
Raghavelu <68513343+Raghavelu@users.noreply.github.com>
2025-05-02 17:09:14 +0530

    Update nixpkgs.nix

--- .nixpacks/nixpkgs.nix
+++ .nixpacks/nixpkgs.nix
@@ -1,23 +1,31 @@
 with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz") {});
+
+let
+  pythonEnv = pkgs.python311.withPackages (ps: [
+    ps.flask
+    ps.openai
+    ps.fpdf2
+    ps.python-dotenv
+    ps.requests
+    ps.gunicorn
+    ps.python-dateutil
+    ps.gevent
+    ps.numpy
+    ps.chardet
+    ps.setuptools
+  ]);
+in
 pkgs.mkShell {
-    buildInputs = [
-        pkgs.python311
-        # Python packages
-        pkgs.python311Packages.flask
-        pkgs.python311Packages.openai
-        pkgs.python311Packages.fpdf2
-        pkgs.python311Packages.python-dotenv
-        pkgs.python311Packages.requests
-        pkgs.python311Packages.gunicorn
-        pkgs.python311Packages.python-dateutil
-        pkgs.python311Packages.gevent
-        pkgs.python311Packages.numpy
-        pkgs.python311Packages.chardet
-        
-        # System dependencies
-        pkgs.dejavu_fonts
-        pkgs.fontconfig
-        pkgs.zlib
-        pkgs.stdenv.cc.cc.lib
-    ];
+  buildInputs = [
+    pythonEnv
+    pkgs.dejavu_fonts
+    pkgs.fontconfig
+    pkgs.zlib
+    pkgs.stdenv.cc.cc.lib
+  ];
+
+  shellHook = ''
+    export PYTHONPATH=${pythonEnv}/${pythonEnv.sitePackages}
+    export PATH="${pythonEnv}/bin:$PATH"
+  '';
 }
