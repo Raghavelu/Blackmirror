98bdccda5d1cbbea25cfa4e78b0ffab58fc6b107
Raghavelu <68513343+Raghavelu@users.noreply.github.com>
2025-05-02 18:24:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
 from dotenv import load_dotenv
+from nixpkgs import pkgs
 
 load_dotenv()  # This loads environment variables from a .env file
+
+
+DEJAVU_FONT_PATH = f"{pkgs.dejavu_fonts}/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+FONT_PATH = os.environ.get('FPDF_FONT_DIR', DEJAVU_FONT_PATH)
+
 
 # Check if the API key is loaded correctly
 OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
@@ -9,3 +15,5 @@ if not OPENROUTER_API_KEY:
     print("[ERROR] API key is missing. Please check your environment settings.")
 else:
     print("[INFO] API key loaded successfully.")
+
+
